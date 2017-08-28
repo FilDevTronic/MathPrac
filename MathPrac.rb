964b87b56cb6aaa86b381f4f1132808e17e04cbe
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-08-28 12:14:22 +1000

    Unthreaded

--- MathPrac.rb
+++ MathPrac.rb
@@ -1,33 +1,31 @@
 num = (1..10).to_a # Creates an Array called num of numbers 1 through and including 10
 
-num1 = num.shuffle[0] # Grabs a number at random from the num Array and sets it as variable num1
-num2 = num.shuffle[0] # Grabs a number at random from the num Array and sets it as variable num2
+num1 = num.sample # Grabs a number at random from the num Array and sets it as variable num1
+num2 = num.sample # Grabs a number at random from the num Array and sets it as variable num2
 
 syms = [:+, :-, :/, :*] # Creates a Hash of operators called syms, stored as symbols
 
 op = syms.sample # Grabs a random operator from the syms Hash, using the sample method, and sets it as variable op
 
-exercise = Thread.new do
-  print "Solve this problem: "
-  work = "#{num1}" + "#{op}" + "#{num2}"
-  calc = eval(work)
-  puts work
-  puts "What is your answer?"
-  answer = gets.chomp.to_f
+puts "Ready to begin: Y/N?"
+
+choose = gets.chomp.upcase!
+
+if choose == "Y"
+    print "Solve this problem: "
+    work = "#{num1}" + "#{op}" + "#{num2}"
+    calc = eval(work)
+    puts work
+    puts "What is your answer?"
+    answer = gets.chomp.to_f
     if calc == answer
       puts "CORRECT"
     else
       puts "INCORRECT"
     end
-end
 
-
-puts "Ready to begin: Y/N?"
-choose = gets.chomp.upcase
-if choose == "Y"
-  exercise
 elsif choose == "N"
-  puts "Ok, nevermind..."
+  puts "Nevermind..."
 else
-  puts "Wrong input! Shutting down."
+  puts "Say what?"
 end
