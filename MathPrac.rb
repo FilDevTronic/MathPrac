efb29ecf956f9bde3d24f57257251977d0c78aa6
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-08-30 21:43:43 +1000

    Math calculation changes
    -Removed calculation using eval() method and replaced it with arg.send(symbol,arg2) method instead.
    
    -Added rounding to two decimal places to avoid users having to enter long floats for answers with repeating numbers, e.g 1.333333333
    
    -Added exit confirmation output after continue prompt

--- MathPrac.rb
+++ MathPrac.rb
@@ -8,15 +8,15 @@ if choose == "Y" # If the user enters Y as their input, runs the loop
   loop do # Starts a loop for the math exercise generator
     num = (1..10).to_a # Creates an Array called num of numbers 1 through and including 10
 
-    num1 = num.sample # Grabs a number at random from the num Array and sets it as variable num1
-    num2 = num.sample # Grabs a number at random from the num Array and sets it as variable num2
+    num1 = num.sample.to_f # Grabs a number at random from the num Array and sets it as variable num1
+    num2 = num.sample.to_f # Grabs a number at random from the num Array and sets it as variable num2
 
     syms = [:+, :-, :/, :*] # Creates a Hash of operators called syms, stored as symbols
 
     op = syms.sample # Grabs a random operator from the syms Hash, using the sample method, and sets it as variable op
     print "Solve this problem: "
     work = "#{num1}" + "#{op}" + "#{num2}"
-    calc = eval(work)
+    calc = num1.public_send(op, num2).round(2)
     puts work
     puts "What is your answer?"
     answer = gets.chomp.to_f
@@ -27,6 +27,7 @@ if choose == "Y" # If the user enters Y as their input, runs the loop
     next if continue == "Y"
     break if continue == "N"
   end
+  puts "Ok, take care!"
 elsif choose == "N"
   puts "Nevermind..."
 else
