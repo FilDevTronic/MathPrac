dfd04d4a22be080c66a65c049951c012fc466f17
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-09-16 22:53:21 +1000

    Added difficulty levels
    Added very quick and dirty code for difficulty level options, numbers 1-10, 1-100 and 1-1000. Will need to change methods into classes to make it more DRY, currently relying on case with repeated methods

--- MathPrac.rb
+++ MathPrac.rb
@@ -7,33 +7,105 @@ begin # Start of program within a rescue/retry loop should user enter unexpected
   choose = gets.chomp.upcase! # Gets user's input as a string and turns it into capitals, then sets it as the choose variable
 
   if choose == "Y" # If the user enters Y as their input, runs the loop
-    loop do # Starts a loop for the math exercise generator
-      num = (1..10).to_a # Creates an Array called num of numbers 1 through and including 10
 
-      num1 = num.sample.to_f # Grabs a number at random from the num Array and sets it as variable num1
-      num2 = num.sample.to_f # Grabs a number at random from the num Array and sets it as variable num2
+    puts "Choose a difficulty level based on highest possible number:"
+    puts "1. Easy - 10"
+    puts "2. Medium - 100"
+    puts "3. Hard - 1000"
 
-      syms = [:+, :-, :/, :*] # Creates a Hash of operators called syms, stored as symbols
+    dif = gets.chomp.to_i
 
-      op = syms.sample # Grabs a random operator from the syms Hash, using the sample method, and sets it as variable op
+    case dif
+    when 1
+      loop do # Starts a loop for the math exercise generator
 
-      print "Solve this problem: "
+        num = (1..10).to_a # Creates an Array called num of numbers 1 through and including 10
 
-      work = "#{num1}" + "#{op}" + "#{num2}" # Takes a random value from num1, a random value from num2, and a random operator from syms and interpolates them into a string
-      calc = num1.public_send(op, num2).round(2)
-      puts work # Print to console the math calculation
+        syms = [:+, :-, :/, :*] # Creates a Hash of operators called 'syms', stored as symbols
 
-      puts "What is your answer?"
-      answer = gets.chomp.to_f # Sets an answer variable as input from the user, converted to a float
+        op = syms.sample # Grabs a random operator from the syms Hash, using the sample method, and sets it as variable op
 
-      puts "CORRECT" if calc == answer # If the answer variable matches the calc variable, tells user they are correct in console
+        num1 = num.sample.to_f # Grabs a number at random from the num Array and sets it as variable num1
+        num2 = num.sample.to_f # Grabs a number at random from the num Array and sets it as variable num2
 
-      puts "INCORRECT, the answer is #{calc}." if calc != answer # If the answer and calc variables don't match, tells user their answer is incorrect and shows the correct answer
+        print "Solve this problem: "
 
-      puts "Do you want to continue? Y/N"
-      continue = gets.chomp.upcase # Get user input to see if to continue with another math problem or to stop the program
-      next if continue == "Y" # If the user input above is "Y", the math problem generator runs again
-      break if continue == "N" # If the user input above is "N", the program exits.
+        work = "#{num1}" + "#{op}" + "#{num2}" # Takes a random value from num1, a random value from num2, and a random operator from syms and interpolates them into a string
+        calc = num1.public_send(op, num2).round(2)
+        puts work # Print to console the math calculation
+
+        puts "What is your answer?"
+        answer = gets.chomp.to_f # Sets an answer variable as input from the user, converted to a float
+
+        puts "CORRECT" if calc == answer # If the answer variable matches the calc variable, tells user they are correct in console
+
+        puts "INCORRECT, the answer is #{calc}." if calc != answer # If the answer and calc variables don't match, tells user their answer is incorrect and shows the correct answer
+
+        puts "Do you want to continue? Y/N"
+        continue = gets.chomp.upcase # Get user input to see if to continue with another math problem or to stop the program
+        next if continue == "Y" # If the user input above is "Y", the math problem generator runs again
+        break if continue == "N" # If the user input above is "N", the program exits.
+      end
+    when 2
+      loop do # Starts a loop for the math exercise generator
+
+        num = (1..100).to_a # Creates an Array called num of numbers 1 through and including 100
+
+        syms = [:+, :-, :/, :*] # Creates a Hash of operators called 'syms', stored as symbols
+
+        op = syms.sample # Grabs a random operator from the syms Hash, using the sample method, and sets it as variable op
+
+        num1 = num.sample.to_f # Grabs a number at random from the num Array and sets it as variable num1
+        num2 = num.sample.to_f # Grabs a number at random from the num Array and sets it as variable num2
+
+        print "Solve this problem: "
+
+        work = "#{num1}" + "#{op}" + "#{num2}" # Takes a random value from num1, a random value from num2, and a random operator from syms and interpolates them into a string
+        calc = num1.public_send(op, num2).round(2)
+        puts work # Print to console the math calculation
+
+        puts "What is your answer?"
+        answer = gets.chomp.to_f # Sets an answer variable as input from the user, converted to a float
+
+        puts "CORRECT" if calc == answer # If the answer variable matches the calc variable, tells user they are correct in console
+
+        puts "INCORRECT, the answer is #{calc}." if calc != answer # If the answer and calc variables don't match, tells user their answer is incorrect and shows the correct answer
+
+        puts "Do you want to continue? Y/N"
+        continue = gets.chomp.upcase # Get user input to see if to continue with another math problem or to stop the program
+        next if continue == "Y" # If the user input above is "Y", the math problem generator runs again
+        break if continue == "N" # If the user input above is "N", the program exits.
+      end
+    when 3
+      loop do # Starts a loop for the math exercise generator
+
+        num = (1..1000).to_a # Creates an Array called num of numbers 1 through and including 1000
+
+        syms = [:+, :-, :/, :*] # Creates a Hash of operators called 'syms', stored as symbols
+
+        op = syms.sample # Grabs a random operator from the syms Hash, using the sample method, and sets it as variable op
+
+        num1 = num.sample.to_f # Grabs a number at random from the num Array and sets it as variable num1
+        num2 = num.sample.to_f # Grabs a number at random from the num Array and sets it as variable num2
+
+        print "Solve this problem: "
+
+        work = "#{num1}" + "#{op}" + "#{num2}" # Takes a random value from num1, a random value from num2, and a random operator from syms and interpolates them into a string
+        calc = num1.public_send(op, num2).round(2)
+        puts work # Print to console the math calculation
+
+        puts "What is your answer?"
+        answer = gets.chomp.to_f # Sets an answer variable as input from the user, converted to a float
+
+        puts "CORRECT" if calc == answer # If the answer variable matches the calc variable, tells user they are correct in console
+
+        puts "INCORRECT, the answer is #{calc}." if calc != answer # If the answer and calc variables don't match, tells user their answer is incorrect and shows the correct answer
+
+        puts "Do you want to continue? Y/N"
+        continue = gets.chomp.upcase # Get user input to see if to continue with another math problem or to stop the program
+        next if continue == "Y" # If the user input above is "Y", the math problem generator runs again
+        break if continue == "N" # If the user input above is "N", the program exits.
+      end
     end
 
   elsif choose == "N" # If the user enters "N", the math program doesn't run and exits instead.
